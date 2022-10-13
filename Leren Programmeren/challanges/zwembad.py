e19f1f92749002669be465cca4182794e7537636
Max <99062843@mydavinci.nl>
2022-10-13 10:06:19 +0200

    Stap 3

--- Leren Programmeren/challanges/zwembad.py
+++ Leren Programmeren/challanges/zwembad.py
@@ -1,8 +1,31 @@
 lengte = float(input("Hoeveel meter lang is het zwembad "))
 breede =float(input("Hoeveel meter breed is het zwembad "))
 hoogte = float(input("Hoeveel meter hoog is het zwembad "))
+afstand = float(input("Hoe ver is het in km"))
+uitgrave = lengte * breede * hoogte
+
+
+if uitgrave <= 20:
+    vaste_prijs = 100
+    if afstand <=50:
+        prijs_per_km = 1.25
+    else:
+        prijs_per_km =  1.15
+else:
+    vaste_prijs = 250
+    if afstand <=50:
+        prijs_per_km = 2.15
+    else:
+        prijs_per_km =  2.05
+
+
+
 
 
 grond_afvoeren = lengte * breede * hoogte
-
-print(grond_afvoeren)
+prijs_grond_afvoeren = grond_afvoeren * 32.5
+prijs_uitgrave = uitgrave * 25
+prijs_afstand = afstand * prijs_per_km 
+totaal = prijs_grond_afvoeren + prijs_uitgrave + vaste_prijs + prijs_afstand
+round_total = round(totaal,2)
+print(f"Totaal: {totaal} € ")
